df0ffd2edc1c1baf8fed262830375b6e7ddca995
tokimo-studio <gusdn883@naver.com>
2026-04-01 15:10:40 +0900

    수정

--- js/project.js
+++ js/project.js
@@ -1,12 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-
   const items = document.querySelectorAll(".collage-item");
 
   items.forEach(item => {
     const img = item.getAttribute("data-img");
     if (img) {
-      item.style.setProperty("--bg", `url(${img})`);
+      // 핵심: CSS 파일이 css/ 폴더 안에 있으므로 상위로 이동(../) 후 이미지를 찾도록 수정
+      item.style.setProperty("--bg", `url('../${img}')`);
     }
   });
-
 });
