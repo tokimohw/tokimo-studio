1cd1969d59ce4b962cfd9f35679a5a6ef7ad943e
tokimo-studio <gusdn883@naver.com>
2026-04-01 14:36:53 +0900

    project

--- js/project.js
+++ js/project.js
@@ -2,9 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const items = document.querySelectorAll(".collage-item");
 
-  /* =========================
-     BACKGROUND IMAGE SET
-  ========================= */
   items.forEach(item => {
     const img = item.getAttribute("data-img");
     if (img) {
